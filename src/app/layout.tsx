1acf6dfeef9df896331c5edda95db728251a1622
David Horjet <davidojetola956@gmail.com>
2024-12-29 21:27:47 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ClientReduxProvider from "@/components/ReduxProvider/ReduxProvider";
 import { Bricolage_Grotesque } from "next/font/google";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { clusterApiUrl } from "@solana/web3.js";
-import { useMemo } from "react";
+// import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+// import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { clusterApiUrl } from "@solana/web3.js";
+// import { useMemo } from "react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
 import { NextAuthProvider } from "@/components/NextAuthProvider/NextAuthProvider";
 
@@ -27,15 +27,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const network = WalletAdapterNetwork.Mainnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  // const network = WalletAdapterNetwork.Mainnet;
+  // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-    ],
-    []
-  );
   return (
     <html lang="en" className="dark">
       <body
